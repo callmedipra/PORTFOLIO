0841afb40192577ea34530132b8fc109cf7bc66d
callmedipra <biswasdipra44@gmail.com>
2024-02-18 18:17:07 +0530

    Added Skills and Education functionalities

--- script.js
+++ script.js
@@ -44,3 +44,42 @@ t1.from(".fa-brands ",{
     duration:0.4,
     stagger:0.2,
 })
+
+let h1s=document.querySelectorAll(".skill-container-heading h1");
+let ps=document.querySelectorAll(".skill-container-wrapper p");
+let skills=document.querySelector(".skills");
+console.log(skills);
+let education=document.querySelector(".education");
+let index=0;
+let animating=false;
+let btn=document.querySelector(".arrow-down");
+btn.addEventListener("click",function(){
+    if(!animating)
+    {
+        animating=true;
+        gsap.to(h1s[index],{
+            top:'-=100%',
+            onComplete:function(){
+                gsap.set(this._targets[0],{
+                    top:100,
+                })
+                animating=false;
+            }
+        })
+    
+        
+        index===h1s.length -1 ? (index=0) : index++;
+        console.log(h1s[index]);
+        gsap.to(h1s[index],{
+            top:'-=100%',
+        })
+        if(index==0){
+            skills.style.display="block";
+            education.style.display="none";
+        }
+        else{
+            education.style.display="block";
+            skills.style.display="none";
+        }
+    }
+})
